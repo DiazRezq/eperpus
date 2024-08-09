0a2e173ae433746448d8128e62061228785cdae3
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-08-10 06:58:05 +0700

    categories page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"data-aos="fade-down">
     <div class="container">
-      <a class="navbar-brand" href="/">
+      <a class="navbar-brand" href="{{ route('home') }}">
         <img src="/images/logoeperpus.svg" alt="" />
       </a>
       <button
@@ -18,10 +18,10 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home </a>
+            <a class="nav-link" href="{{ route('home') }}">Home </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/categories.html">Categories</a>
+            <a class="nav-link" href="{{ route('categories') }}">Categories</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Rewards</a>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);

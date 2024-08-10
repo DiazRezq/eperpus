74e9a704287bc2362f3af2caf554e48c24bb0dcf
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-08-10 07:07:57 +0700

    fix category

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -21,7 +21,7 @@
             <a class="nav-link" href="{{ route('home') }}">Home </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('categories') }}">Categories</a>
+            <a class="nav-link" href="{{ route('category') }}">Categories</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Rewards</a>
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/category', [CategoryController::class, 'index'])->name('category');

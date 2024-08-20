b43cd61475c2bc8c6b39e61394779a3a06d27083
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2024-08-20 21:33:26 +0700

    backend setup

---
+++ database/migrations/2024_08_20_140609_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('user_id');
+            $table->string('isbn')->unique();
+            $table->string('author');
+            $table->integer('published_year');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('publisher_id');
+            $table->string('cover_image')->nullable();
+            $table->string('description')->nullable();
+            $table->integer('stock');
+            $table->integer('rental_price');
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('publisher_id')->references('id')->on('publishers')->onDelete('cascade');
+            $table->softDeletes();
+            $table->engine('innoDB');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
